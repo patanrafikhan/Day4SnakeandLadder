aad6cba16d19b68a52aa6aec184bd404f8c3924c
patanrafikhan <iamkhan342@gmail.com>
2023-01-30 17:30:36 +0530

    UC4 Snake and Ladder Program

---
+++ SnakeandLadderUC4/SnakeandLadderUC4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SnakeandLadderUC4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sanke and Ladder game");
+            int position = 0;
+            int NoOfDieroll = 0;
+            Random ran = new Random();
+            while(position<=100)
+            {
+                int number = ran.Next(0, 7);
+                NoOfDieroll++;
+                Console.WriteLine("Number of die rolls : " + NoOfDieroll);
+                int option = ran.Next(0, 3);
+
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("No play");
+                        Console.WriteLine("The Position is : " + position);
+                        break;
+                    case 1:
+                        Console.WriteLine("Ladder");
+                        position = position + number;
+                        Console.WriteLine("The Position is : " + position);
+                        break;
+                    case 2:
+                        Console.WriteLine("Snake");
+                        position = position + number;
+                        Console.WriteLine("The Position is : " + position);
+                        break;
+
+                }
+            }
+            
+        }
+    }
+}
